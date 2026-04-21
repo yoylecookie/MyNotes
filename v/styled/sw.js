5ae536a3e1fc3abd3a77e48667e6209d79994aa2
YoyleCookie <yoyle_cookie@proton.me>
2026-04-20 21:24:37 -0400

    Update sw.js

--- v/styled/sw.js
+++ v/styled/sw.js
@@ -1,23 +1,19 @@
-const CACHE_NAME = "mynotes-cache-v1";
+const CACHE_NAME = "mynotes-cache-v2";
 const urlsToCache = [
-  "/MyNotes/",
-  "/MyNotes/index.html",
-  "/MyNotes/manifest.json",
-  "https://yoylecookie.github.io/MyNotes/logo.png"
+  "/MyNotes/v/styled/",
+  "/MyNotes/v/styled/index.html",
+  "/MyNotes/v/styled/manifest.json",
+  "/MyNotes/v/styled/logo.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
